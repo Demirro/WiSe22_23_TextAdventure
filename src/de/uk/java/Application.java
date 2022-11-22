0a39cbf6809aa83d2e4caf4925627f2c95e7118d
Demirro <theo.bouveyron@gmx.de>
2022-11-22 09:25:13 +0100

    added attack

--- src/de/uk/java/Application.java
+++ src/de/uk/java/Application.java
@@ -1,10 +1,11 @@
 package de.uk.java;
 
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		// Variablen für Spielende
 		int playerHealth = 100;
@@ -20,7 +21,7 @@ public class Application {
 		playerName = input.nextLine(); // auf User-Input (neue Zeile) warten
 		
 		// Game-Loop starten, läuft ewig, es sei den man gibt exit ein
-		while (true) {
+		gameloop : while (true) {
 			System.out.println("Gebe einen Befehl ein: ");
 
 			// Kommandoeingabe abwarten
@@ -38,9 +39,32 @@ public class Application {
 			case "show player":
 				System.out.printf("Name: %s\nHealth: %s\nStrength: %s\n", playerName, playerHealth, playerStrength);
 				break;
+			case "attack":
+				int enemyHealth = 10;
+				int enemyStrength = 2;
+				attackLoop : while (true) {
+					if (enemyHealth <= 0) {
+						System.out.println("Der Gegner ist gestorben. Du hast gewonnen");
+						break attackLoop;
+					} else if (playerHealth <= 0) {
+						System.out.println("Du bist gestorben");
+						break attackLoop;
+					}
+					System.out.printf("Du greifst mit %s Stärke an.\n", playerStrength);
+					enemyHealth -= playerStrength;
+					TimeUnit.MILLISECONDS.sleep(500);
+					System.out.printf("Der Gegner hat noch %s Leben.\n", enemyHealth);
+					TimeUnit.MILLISECONDS.sleep(1000);
+					System.out.printf("Der Gegner greift dich mit %s Stärke an.\n", enemyStrength);
+					TimeUnit.MILLISECONDS.sleep(500);
+					playerHealth -= enemyStrength;
+					System.out.printf("Du hast noch %s Leben.\n", playerHealth);
+					TimeUnit.MILLISECONDS.sleep(1000);
+				}
+				break;
 			case "exit":
 				System.out.println("Schade, dass du jetzt schon gehen möchtest. Tschüss!");
-				System.exit(0);
+				break gameloop;
 			default:
 				System.out.println("Den Befehl gibt es nicht!");
 			}
